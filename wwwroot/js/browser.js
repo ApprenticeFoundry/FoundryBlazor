e946c571c8404be50e880c865eb7c6fd69d4834e
Greg Fulton <gtfullton@gmail.com>
2023-03-15 17:17:26 -0400

    new methods for pdf

--- wwwroot/js/browser.js
+++ wwwroot/js/browser.js
@@ -5,5 +5,16 @@ class Browser {
             innerHeight: window.innerHeight,
         };
     }
+
+    canvasPNGBase64(id = 'canvasHolder') {
+        const containerNode = document.getElementById(id);
+        let canvasNode = null;
+        if (Boolean(containerNode)) canvasNode = containerNode.getElementsByTagName('canvas').item(0);
+
+        if (Boolean(canvasNode)) {
+            console.log('canvasNode=', canvasNode);
+            return canvasNode.toDataURL();
+        }
+    }
 }
 window.Browser = new Browser();
