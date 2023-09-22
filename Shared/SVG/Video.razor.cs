000b43acd2fdea9c1dc915adaaf3eff43964eef3
Greg Fulton <gtfullton@gmail.com>
2023-09-22 15:02:04 -0400

    got video to work

---
+++ Shared/SVG/Video.razor.cs
@@ -0,0 +1,37 @@
+using FoundryBlazor.Shape;
+using FoundryRulesAndUnits.Extensions;
+using Microsoft.AspNetCore.Components;
+using System.Text;
+
+namespace FoundryBlazor.Shared.SVG;
+
+public class VideoBase : SVGBase<FoVideo2D>
+{
+    [Inject] protected ISelectionService? SelectionService { get; set; }
+
+    [Parameter] public FoVideo2D Shape { get; set; } = new();
+    protected int PaddingX { get; set; } = 12;
+    protected int PaddingY { get; set; } = 12;
+    private FoVideo2D GetVideoShape()
+    {
+        return Shape as FoVideo2D;
+    }
+
+    protected string GetURL()
+    {
+        return Shape.ImageUrl;
+    }
+
+    protected string GetStyle()
+    {
+        var style = new StringBuilder("width:").Append(GetWidth() - 2 * PaddingX).Append("px;").Append("height:").Append(GetHeight() - 2 * PaddingY).Append("px").ToString();
+        return style;
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        InitSource(Shape);
+    }
+
+}
