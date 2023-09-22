f2e9b4b06fd175aef984de0d214333d0442b8e87
Greg Fulton <gtfullton@gmail.com>
2023-09-22 15:25:01 -0400

    add image

--- Shared/SVG/Image.razor.cs
+++ Shared/SVG/Image.razor.cs
@@ -1,15 +1,29 @@
 using FoundryBlazor.Shape;
 using FoundryRulesAndUnits.Extensions;
+using FoundryRulesAndUnits.Models;
 using Microsoft.AspNetCore.Components;
 using System.Text;
 
 namespace FoundryBlazor.Shared.SVG;
 
-public class VideoBase : SVGBase<FoVideo2D>
+public class ImageBase : SVGBase<FoImage2D>
 {
+    public static (int width, int height, string url) RandomSpec()
+    {
+        var gen = new MockDataGenerator();
+        var width = 50 * gen.GenerateInt(1, 6);
+        var height = 50 * gen.GenerateInt(1, 6);
+
+        return (
+            width,
+            height,
+            $"https://picsum.photos/{width}/{height}"
+        );
+    }
+
     [Inject] protected ISelectionService? SelectionService { get; set; }
 
-    [Parameter] public FoVideo2D Shape { get; set; } = new();
+    [Parameter] public FoImage2D Shape { get; set; } = new();
     protected int PaddingX { get; set; } = 12;
     protected int PaddingY { get; set; } = 12;
 
