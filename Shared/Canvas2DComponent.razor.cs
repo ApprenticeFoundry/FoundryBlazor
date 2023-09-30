51f86d54d2a20ff74bd1ebd4a370c82709d58524
Steve <stephen.r.strong@saic.com>
2023-09-30 00:02:40 -0400

    ok

--- Shared/Canvas2DComponent.razor.cs
+++ Shared/Canvas2DComponent.razor.cs
@@ -83,7 +83,7 @@ public class Canvas2DComponentBase : ComponentBase
         }
         tick++;
 
-        $"Canvas2D RenderFrame {tick} {fps}".WriteInfo();
+        //$"Canvas2D RenderFrame {tick} {fps}".WriteInfo();
 
         Workspace?.PreRender(tick);
 
